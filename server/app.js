191eef4889cd4d129245a7269dda54833ca1cf5a
isabela tassia <isabelatassiasilva@gmail.com>
2021-05-11 08:24:31 -0300

    done

--- server/app.js
+++ server/app.js
@@ -1,36 +1,28 @@
 require("dotenv").config();
 console.log(__dirname + "/.env");
 const express = require("express");
-const bodyParser = require("body-parser");
+customElements bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express();
 
-// Configurar nosso app Express para entender requisições com conteúdo JSON
 app.use(express.json());
-
-// Configurar nosso app para entender requisições do tipo URLEncoded (para envio de imagens)
 app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use(bodyParser.json());
 app.use(cors({ origin: "http://localhost:3000" }));
 
-// Importar a configuração do banco de dados (mongoose)
 const db = require("./config/db.config.js");
-// Invoca a função que realiza a conexão com o banco de dados
 db();
 
-// Importar os roteadores
-const router = require("./routes/index.routes");
+const router = require('./routes/index.routes');
 app.use("/", router);
-
 const roomRouter = require("./routes/room.routes");
-app.use("/", roomRouter);
 
-const reviewRouter = require("./routes/review.routes");
+app.use('/', roomRouter);
+
+const reviewRouter = require('/routes/review.routes');
 app.use("/", reviewRouter);
 
-// Subir o servidor web para escutar requisições
-
 app.listen(Number(process.env.PORT), () =>
-  console.log(`Server up and running at port ${process.env.PORT}`)
+  console.log(`Server working at port ${process.env.PORT}`)
 );
--- server/models/Room.model.js
+++ server/models/Room.model.js
@@ -1,14 +1,11 @@
 const mongoose = require("mongoose");
-
 const Schema = mongoose.Schema;
-
 const roomSchema = new Schema({
   name: { type: String },
   description: { type: String },
   imageUrl: { type: String },
-  reviews: [{ type: Schema.Types.ObjectId, ref: "Review" }],
+  reviews: [],
 });
 
 const RoomModel = mongoose.model("Room", roomSchema);
-
 module.exports = RoomModel;
